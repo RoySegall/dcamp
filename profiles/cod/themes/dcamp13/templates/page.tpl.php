61f1d5ece4cd029807dcdbef825e14bb2c6bf22e
RoySegall <roy.segall@gizra.com>
2013-08-09 20:25:38 +0300

    Adding slideshow

--- profiles/cod/themes/dcamp13/templates/page.tpl.php
+++ profiles/cod/themes/dcamp13/templates/page.tpl.php
@@ -28,153 +28,28 @@
         </ul>
       </div>
     </div>
-    <div id="main">
-      <div class="carousel">
-        <div class="mask">
-          <div class="slideset">
-            <div class="slide"><img src="images/img-01.jpg" width="940" height="390" alt="image description" /></div>
-            <div class="slide"><img src="images/img-01.jpg" width="940" height="390" alt="image description" /></div>
-            <div class="slide"><img src="images/img-01.jpg" width="940" height="390" alt="image description" /></div>
-          </div>
-        </div>
-        <a class="btn-prev" href="#">Previous</a>
-        <a class="btn-next" href="#">Next</a>
-      </div>
-      <div class="social-networks">
-        <ul>
-          <li><a class="mail" href="#">mail</a></li>
-          <li><a class="drupal" href="#">drupal</a></li>
-          <li><a class="facebook" href="#">facebook</a></li>
-          <li><a class="linkedin" href="#">linkedin</a></li>
-        </ul>
-      </div>
-      <div class="container-holder">
-        <div class="container">
-          <div class="photo-holder">
-            <img src="images/img-02.png" width="184" height="184" alt="image description" />
-          </div>
-          <div class="description">
-            <h2><a href="#">בואו להכיר את האורחים</a></h2>
-            <ul class="img-list">
-              <li>
-                <a href="#"><img src="images/img-06.jpg" width="145" height="145" alt="image description" /></a>
-                <div class="text-holder">
-                  <div class="text-frame">
-                    <strong><a href="#">Jonathan Sacksick</a></strong>
-                    <span>Commerce Guy</span>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <a href="#"><img src="images/img-05.jpg" width="145" height="145" alt="image description" /></a>
-                <div class="text-holder">
-                  <div class="text-frame">
-                    <strong><a href="#">Jonathan Sacksick</a></strong>
-                    <span>Commerce Guy</span>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <a href="#"><img src="images/img-06.jpg" width="145" height="145" alt="image description" /></a>
-                <div class="text-holder">
-                  <div class="text-frame">
-                    <strong><a href="#">Jonathan Sacksick</a></strong>
-                    <span>Commerce Guy</span>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <a href="#"><img src="images/img-05.jpg" width="145" height="145" alt="image description" /></a>
-                <div class="text-holder">
-                  <div class="text-frame">
-                    <strong><a href="#">Jonathan Sacksick</a></strong>
-                    <span>Commerce Guy</span>
-                  </div>
-                </div>
-              </li>
-            </ul>
-            <p>בין האורחים המכובדים שהבטיחו השתתפותם בכנס: השר מיכאל איתן, שהינו השר הממונה על  שיפור השירות לציבור, אשר מקדיש תשומת לב מיוחדת לצמצום הפער הדיגיטלי ולקידום <br />שירותי ממשל זמין לטובת ציבור האזרחים. <br /> ג'פרי מקגוויר ("jam"), מנהל קשרי קהילה בחברת אקוויה, אשר ירצה בנושא “ The Real  <span>Value of Open Source: ROI and beyond." <a href="#">   עוד...</a></span></p>
-          </div>
-        </div>
-        <div class="container">
-          <div class="photo-holder">
-            <img src="images/img-03.png" width="184" height="184" alt="image description" />
-          </div>
-          <div class="description">
-            <h2><a href="#">קול קורא לנותני חסות</a></h2>
-            <div class="logos-holder">
-              <a href="#"><img class="alignleft" src="images/logo-01.png" width="217" height="72" alt="Google" /></a>
-              <a href="#"><img class="alignright" src="images/logo-02.png" width="257" height="51" alt="ACQUIA" /></a>
-            </div>
-            <p>מספר המבקרים באירוע מוערך נכון להיום במאות. <br /> מטרת כנס דרופלקאמפ - לערוך היכרות עם המערכת עם משתתפים חדשים, לפתח את קשרי הקהילה  הנבנית בישראל, ולהעלות נושאים לדיון למתחילים ומתקדמים. מיותר לציין שהכנס מפורסם במספר אתרים מרכזיים העוסקים בתחומי טכנולוגיה, פיתוח,  <br /> <span>בניית אתרים, עיצוב, <a href="#">  עוד...</a></p>
-          </div>
-        </div>
-        <div class="container">
-          <div class="photo-holder">
-            <img src="images/img-04.png" width="184" height="184" alt="image description" />
-          </div>
-          <div class="description">
-            <h2><a href="#">מהר מהר לפני שייגמר</a></h2>
-            <div class="holder">
-              <a href="#" class="button">להרשמה </a>
-              <strong class="title">למה אתם מחכים?</strong>
-            </div>
-            <p>מבחינת פעילות הדרופל הענפה בארץ, התוכן האיכותי הצפוי מהארץ ומהעולם ולעומת  כ-200-300 המקומות של הכנס יש מצב סביר שייגמרו הכרטיסים. תפסו את המחיר המוזל (עד ה לאוגוסט) בהרשמה מוקדמת ושמרו מקום - לכם, לעובדיכם וללקוחתיכם<a href="#"> עוד...</a></p>
-          </div>
-        </div>
-      </div>
-      <div class="container additional">
-        <div class="photo-holder">
-          <img src="images/img-05.png" width="184" height="184" alt="image description" />
-        </div>
-        <div class="description">
-          <h2><a href="#">הכנס בחסות</a></h2>
-          <div class="logo-container">
-            <a href="#"><img src="images/logo-01.png" width="217" height="72" alt="Google" class="alignleft" /></a>
-            <a href="#"><img src="images/logo-06.png" width="241" height="48" alt="ACQUIA" class="alignright" /></a>
-          </div>
-          <ul class="company-list">
-            <li>
-              <h3>חסות זהב</h3>
-              <div class="logos-holder">
-                <ul>
-                  <li><a href="#"><img src="images/logo-03.png" width="238" height="82" alt="News geek" /></a></li>
-                  <li><a href="#"><img src="images/logo-04.png" width="235" height="55" alt="Incapsula" /></a></li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <h3>חסות כסף</h3>
-              <div class="logos-holder">
-                <ul>
-                  <li><a href="#"><img src="images/logo-03.png" width="238" height="82" alt="News geek" /></a></li>
-                  <li><a href="#"><img src="images/logo-04.png" width="235" height="55" alt="Incapsula" /></a></li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <h3>חסות ארד</h3>
-              <div class="logos-holder">
-                <ul>
-                  <li><a href="#"><img src="images/logo-03.png" width="238" height="82" alt="News geek" /></a></li>
-                  <li><a href="#"><img src="images/logo-04.png" width="235" height="55" alt="Incapsula" /></a></li>
-                </ul>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div id="footer">
-      <div class="footer-holder">
-        <div class="design">
-          <strong class="title">Design by:</strong>
-          <a href="#"><img src="images/logo-05.png" width="96" height="49" alt="Studio Octavo" /></a>
-        </div>
-        <div class="holder">
-          <p>כל הזכויות שמורות לדרופל ישראל</p>
-        </div>
-      </div>
-    </div>
+    <section class="<?php print _bootstrap_content_span($columns); ?>">
+      <?php if (!empty($page['highlighted'])): ?>
+        <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
+      <?php endif; ?>
+      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if (!empty($title)): ?>
+<!--        <h1 class="page-header">--><?php //print $title; ?><!--</h1>-->
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php if (!empty($tabs)): ?>
+        <?php print render($tabs); ?>
+      <?php endif; ?>
+      <?php if (!empty($page['help'])): ?>
+        <div class="well"><?php print render($page['help']); ?></div>
+      <?php endif; ?>
+      <?php if (!empty($action_links)): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+    </section>
   </div>
 </div>
