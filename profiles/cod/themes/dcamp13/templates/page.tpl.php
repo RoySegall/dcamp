7fdfecbd68be27ff0540eba1a1a4ba0daeff5b66
RoySegall <roy.segall@gizra.com>
2013-08-10 18:39:45 +0300

    Start working on the user page and add twitter integration

--- profiles/cod/themes/dcamp13/templates/page.tpl.php
+++ profiles/cod/themes/dcamp13/templates/page.tpl.php
@@ -38,17 +38,11 @@
       </div>
     </div>
     <div id="main">
-      <section class="<?php print !drupal_is_front_page() ? _bootstrap_content_span($columns) : ''; ?>">
+      <section class="<?php print !drupal_is_front_page() ? 'in-page' : ''; ?>">
         <?php if (!empty($page['highlighted'])): ?>
           <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
         <?php endif; ?>
-        <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
         <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if (!empty($title)): ?>
-<!--          <h1 class="page-header">--><?php //print $title; ?><!--</h1>-->
-        <?php endif; ?>
-        <?php print render($title_suffix); ?>
         <?php print $messages; ?>
         <?php if (!empty($tabs)): ?>
           <?php print render($tabs); ?>
