71f7bf59046aac6715fb5efbee630dcddad0d88a
RoySegall <roy.segall@gizra.com>
2013-08-25 23:06:51 +0300

    Fixing markup service CSS. Took me 5 days for that!

--- profiles/cod/themes/dcamp13/templates/page.tpl.php
+++ profiles/cod/themes/dcamp13/templates/page.tpl.php
@@ -32,25 +32,30 @@
         <?php endif; ?>
         <?php print render($page['content']); ?>
       </section>
-      <div class="container additional">
-        <div class="photo-holder">
-          <img src="<?php print $variables['pictures_path']; ?>/images/img-05.png" width="184" height="184" alt="image description" />
-        </div>
-        <div class="description">
-          <h2><a href="#">שותפים</a></h2>
-          <div class="logo-container">
-            <a href="http://www.mit-forum.org.il/"><img src="<?php print $variables['pictures_path']; ?>/images/MIT logo_Heb.jpg" width="217" height="72" alt="Google" class="alignleft" /></a>
-            <a href="http://www.hamakor.org.il/"><img src="<?php print $variables['pictures_path']; ?>/images/Hamakor_logo.png" width="241" height="48" alt="ACQUIA" class="alignright" /></a>
-          </div>
-          <ul class="company-list">
-            <li><?php print $gold_sponsors; ?></li>
-            <li><?php print $silver_sponsors; ?></li>
-            <li><?php print $bronze_sponsors; ?></li>
-          </ul>
-        </div>
-      </div>
+    </div>
   </div>
   <div id="footer">
+    <div class="container-wrapper">
+      <div class="container additional">
+        <div class="additional-inner">
+          <div class="photo-holder">
+            <img src="<?php print $variables['pictures_path']; ?>/images/img-05.png" width="184" height="184" alt="image description" />
+          </div>
+          <div class="description">
+            <h2><a href="#">שותפים</a></h2>
+            <div class="logo-container">
+              <a href="http://www.mit-forum.org.il/"><img src="<?php print $variables['pictures_path']; ?>/images/MIT logo_Heb.jpg" width="217" height="72" alt="Google" class="alignleft" /></a>
+              <a href="http://www.hamakor.org.il/"><img src="<?php print $variables['pictures_path']; ?>/images/Hamakor_logo.png" width="241" height="48" alt="ACQUIA" class="alignright" /></a>
+            </div>
+            <ul class="company-list">
+              <li><?php print $gold_sponsors; ?></li>
+              <li><?php print $silver_sponsors; ?></li>
+              <li><?php print $bronze_sponsors; ?></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="footer-holder">
       <div class="design">
         <strong class="title">Design by:</strong>
